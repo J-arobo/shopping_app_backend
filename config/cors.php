15b40b4ed20198b94bcd2a53033e422191000fa8
J-arobo <arobojoseph4@gmail.com>
2026-06-22 16:52:37 +0300

    Restrict CORS to Firebase frontend

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://food-delivery-app-ce205.web.app'],
 
     'allowed_origins_patterns' => [],
 
