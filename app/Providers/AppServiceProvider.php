7ced1ad06fbdba3c9fce92f7952519226632ac9d
J-arobo <arobojoseph4@gmail.com>
2026-06-23 09:59:44 +0300

    Use Passport native env var keys, publish passport config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('PASSPORT_PRIVATE_KEY_B64') && env('PASSPORT_PUBLIC_KEY_B64')) {
-            file_put_contents(storage_path('oauth-private.key'), str_replace("\r\n", "\n", base64_decode(env('PASSPORT_PRIVATE_KEY_B64'))));
-            file_put_contents(storage_path('oauth-public.key'), str_replace("\r\n", "\n", base64_decode(env('PASSPORT_PUBLIC_KEY_B64'))));
-        }
+        //
     }
 }
